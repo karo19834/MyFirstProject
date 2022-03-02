c4616e6ea99e1a01a594aaa76ff41b03ac79226c
karo19834 <karolina1983_4@hotmail.com>
2022-03-02 11:05:15 +0100

    Initial commit

---
+++ src/fh/campus02/LivingRoom.java
@@ -0,0 +1,17 @@
+package fh.campus02;
+
+public class LivingRoom {
+
+    public static void main(String[] args) {
+        // write your code here
+        double squareMeters = 30.7;
+        boolean hasTv = false;
+        boolean hasCouch = true;
+        int windows = 5;
+        System.out.println("Mein Wohnzimmer hat " + squareMeters + " quadrat Meter, und " + windows + " Fenster. Er hat einen TV- " +
+                hasTv + ". Und einen Couch- " + hasCouch+".");
+
+        
+
+    }
+}
